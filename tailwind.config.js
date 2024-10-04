fce00528d3fa38efcee43337d78c748ae2029379
Andor Gunczer <80578738+AndorGunczer@users.noreply.github.com>
2024-10-04 17:45:05 +0200

    header base

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'green-button': '#007300',
+      },
+      borders: {
+        'border-1': '1px',
+      },
+    },
+  },
+  plugins: [],
+}
+
