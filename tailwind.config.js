d685a0a1693ef4cb7cada199a169ca5b25b636f2
Andor Gunczer <80578738+AndorGunczer@users.noreply.github.com>
2024-10-11 07:08:25 +0200

    Finish mobile html css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
     extend: {
       colors: {
         'green-button': '#007300',
+        'velo-kaki': '#EAE2D7',
+        'grey-500': '#6b7280',
+        'grey-200': '#e5e7eb',
+        'calm-green': '#5c6e5c',
+        'footer-green': '#252A25',
       },
       borders: {
         'border-1': '1px',
