c27ebc5e59c2b4df671aa9d637bff4b95253cfed
Andor Gunczer <80578738+AndorGunczer@users.noreply.github.com>
2024-10-17 21:14:49 +0200

    add typescript

--- website/app/src/js/index.ts
+++ website/app/src/js/index.ts
@@ -1,6 +1,7 @@
-var logosContainer = document.querySelector('.logos');
-var images = Array.from(document.querySelectorAll('.logos-slide img')); // Convert NodeList to Array
-window.addEventListener('load', function () {
+const logosContainer = document.querySelector('.logos');
+const images = Array.from(document.querySelectorAll('.logos-slide img')); // Convert NodeList to Array
+
+window.addEventListener('load', function() {
     if (logosContainer == null) {
         console.error("FATAL: logosContainer is NULL");
         return;
@@ -8,58 +9,70 @@ window.addEventListener('load', function () {
     // Scroll to the middle of the container
     logosContainer.scrollLeft = (logosContainer.scrollWidth - logosContainer.clientWidth) / 2;
 });
+
 function highlightCenterImage() {
     if (logosContainer == null) {
         console.error("FATAL: logosContainer is NULL");
         return;
     }
+
     // Check if all elements in 'images' are HTMLElements
-    if (!images.every(function (img) { return img instanceof HTMLElement; })) {
+    if (!images.every(img => img instanceof HTMLElement)) {
         console.error("FATAL: Not all images are HTMLElement.");
         return;
     }
-    var containerCenter = logosContainer.scrollLeft + logosContainer.clientWidth / 2;
-    var closestImage = null;
-    var closestDistance = Infinity;
-    var closestIndex = -1;
+
+    const containerCenter = logosContainer.scrollLeft + logosContainer.clientWidth / 2;
+    
+    let closestImage = null;
+    let closestDistance = Infinity;
+    let closestIndex = -1;
+
     // Find the image closest to the center
-    images.forEach(function (img, index) {
-        var imgCenter = img.offsetLeft + img.clientWidth / 2;
-        var distance = Math.abs(containerCenter - imgCenter);
+    images.forEach((img, index) => {
+
+        const imgCenter = img.offsetLeft + img.clientWidth / 2;
+        const distance = Math.abs(containerCenter - imgCenter);
+
         if (distance < closestDistance) {
             closestDistance = distance;
             closestImage = img;
             closestIndex = index; // Store the index of the closest image
         }
     });
+
     // Remove active and near-active classes from all images
-    images.forEach(function (img) {
+    images.forEach((img) => {
         img.classList.remove('active');
         img.classList.remove('near-active');
     });
+
     // Add active class to the closest image
     if (closestImage instanceof HTMLElement) {
         closestImage.classList.add('active');
     }
+
     // Add near-active class to the two images next to the center image
     if (closestIndex > 0) { // Check for previous image
-        var prevImage = images[closestIndex - 1];
+        const prevImage = images[closestIndex - 1];
         if (prevImage instanceof HTMLElement) {
             prevImage.classList.add('near-active'); // Previous image
         }
     }
+
     if (closestIndex < images.length - 1) { // Check for next image
-        var nextImage = images[closestIndex + 1];
+        const nextImage = images[closestIndex + 1];
         if (nextImage instanceof HTMLElement) {
             nextImage.classList.add('near-active'); // Next image
         }
     }
 }
+
 if (logosContainer == null) {
     console.error("FATAL: logosContainer is NULL");
-}
-else {
+} else {
     logosContainer.addEventListener('scroll', highlightCenterImage);
 }
+
 // Call the function once on page load to initialize
 window.addEventListener('load', highlightCenterImage);
--- website/app/src/js/nav.js
+++ website/app/src/js/nav.js
@@ -1,16 +1,14 @@
-window.addEventListener("scroll", () => {
-    const scrollingElement = (function () {
-      return document.scrollingElement || document.documentElement;
+window.addEventListener("scroll", function () {
+    var scrollingElement = (function () {
+        return document.scrollingElement || document.documentElement;
     })();
-  
-    const scrollPosition = scrollingElement.scrollTop;
-    const navElement = document.getElementsByTagName("nav")[0];
-  
+    var scrollPosition = scrollingElement.scrollTop;
+    var navElement = document.getElementsByTagName("nav")[0];
     console.log(scrollPosition);
-  
     if (scrollPosition > 72) {
-      navElement.setAttribute("class", "nav-fixed");
-    } else {
-      navElement.setAttribute("class", "nav-static");
+        navElement.setAttribute("class", "nav-fixed");
     }
-  });
+    else {
+        navElement.setAttribute("class", "nav-static");
+    }
+});
