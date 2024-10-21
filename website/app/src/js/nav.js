9a019bdb285b367124c353536f7cadeee23f2bb5
Andor Gunczer <80578738+AndorGunczer@users.noreply.github.com>
2024-10-21 19:40:00 +0200

    implement pdf integration and fix nav visibility settings

--- website/app/src/js/nav.js
+++ website/app/src/js/nav.js
@@ -4,11 +4,14 @@ window.addEventListener("scroll", function () {
     })();
     var scrollPosition = scrollingElement.scrollTop;
     var navElement = document.getElementsByTagName("nav")[0];
+    var imgElement = document.getElementById("velo-mini");
     console.log(scrollPosition);
     if (scrollPosition > 72) {
+        imgElement.setAttribute("class", "img-fixed h-16 w-24 pl-8 py-4");
         navElement.setAttribute("class", "nav-fixed");
     }
     else {
+        imgElement.setAttribute("class", "img-static h-16 w-24 pl-8 py-4");
         navElement.setAttribute("class", "nav-static");
     }
 });
--- website/app/src/js/nav.ts
+++ website/app/src/js/nav.ts
@@ -5,12 +5,15 @@ window.addEventListener("scroll", () => {
   
     const scrollPosition = scrollingElement.scrollTop;
     const navElement = document.getElementsByTagName("nav")[0] as HTMLElement;
+    const imgElement = document.getElementById("velo-mini") as HTMLElement;
   
     console.log(scrollPosition);
   
     if (scrollPosition > 72) {
+      imgElement.setAttribute("class", "img-fixed h-16 w-24 pl-8 py-4");
       navElement.setAttribute("class", "nav-fixed");
     } else {
+      imgElement.setAttribute("class", "img-static h-16 w-24 pl-8 py-4");
       navElement.setAttribute("class", "nav-static");
     }
   });
