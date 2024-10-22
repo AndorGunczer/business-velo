cbaffcdc89a742a46761867c8799be47dcda1a15
Andor Gunczer <80578738+AndorGunczer@users.noreply.github.com>
2024-10-22 18:40:24 +0200

    failed attempt to implement scrolling hamburger menu

--- website/app/src/js/nav.js
+++ website/app/src/js/nav.js
@@ -5,13 +5,16 @@ window.addEventListener("scroll", function () {
     var scrollPosition = scrollingElement.scrollTop;
     var navElement = document.getElementsByTagName("nav")[0];
     var imgElement = document.getElementById("velo-mini");
+    var mobileNav = document.getElementById("mobile-nav");
     console.log(scrollPosition);
     if (scrollPosition > 72) {
         imgElement.setAttribute("class", "img-fixed h-16 w-24 pl-8 py-4");
         navElement.setAttribute("class", "nav-fixed");
+        mobileNav.setAttribute("class", "block lg:hidden p-4 mb-16 hamburger-fixed");
     }
     else {
         imgElement.setAttribute("class", "img-static h-16 w-24 pl-8 py-4");
         navElement.setAttribute("class", "nav-static");
+        mobileNav.setAttribute("class", "block lg:hidden p-4 mb-16 hamburger-static");
     }
 });
