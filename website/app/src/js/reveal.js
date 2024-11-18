ce1eb560847dc9b783fe97d24b760380054cd424
Andor Gunczer <80578738+AndorGunczer@users.noreply.github.com>
2024-11-18 20:37:02 +0100

    animations on the main site

--- website/app/src/js/reveal.js
+++ website/app/src/js/reveal.js
@@ -1,15 +1,38 @@
+const reveals = document.getElementsByTagName("section");
+const revealsChildren = [];
+
+for (let i = 0; i < reveals.length; i++) {
+    revealsChildren.push(Array.from(reveals[i].children));
+    
+}
+
+console.log(revealsChildren);
+
 window.addEventListener("scroll", reveal);
 
 function reveal() {
-    const reveals = document.querySelectorAll(".reveal, .reveal-left, .reveal-right");
 
     for (let i = 0; i < reveals.length; i++) {
         let windowHeight = window.innerHeight;
         let revealTop = reveals[i].getBoundingClientRect().top;
         let revealPoint = 50;
 
+        console.log(`REVEALS[i]: ${reveals[i]}`);
+
         if (revealTop < windowHeight - revealPoint) {
-            reveals[i].classList.add("show");
+            // reveals[i].classList.add("show");
+            // console.log(`REVEALSCHILDREN[i] LENGTH: ${revealsChildren[i].length}`);
+            // console.log(`VALUE OF I: ${i}`);
+            for (let j = 0; j < revealsChildren[i].length; j++) {
+                // console.log(`revealsChildren[i][j]: ${revealsChildren[i][j]}`);
+                // console.log(`revealsChildren[i][j] TYPE: ${revealsChildren[i][j].nodeName}`);
+                if (revealsChildren[i][j].nodeName == "A")
+                    revealsChildren[i][j].children[0].classList.add("show"); 
+                else
+                    revealsChildren[i][j].classList.add("show"); // K IS NOT YET IN!!
+                // if (revealsChildren[i][j])
+                
+            }
         }
     }
 }
